da24fc2909f05f31a3eb825731e2254e4c37802e
Ryan Bulcher <ryanbulcher@Ryans-MacBook-Pro.local>
2021-02-18 20:10:43 -0500

    Updating server.js to have express app listen to port

--- server.js
+++ server.js
@@ -1,4 +1,3 @@
-const http = require('http');
 const express = require('express');
 const path = require('path');
 const app = express();
@@ -12,7 +11,5 @@ app.use('/', function(req,res){
 
 
 
-const server = http.createServer(app);
 const port = 3000;
-server.listen(port || process.env.PORT);
-console.debug('Server listening on port ' + port);
+app.listen(port || process.env.PORT);

8b71ec06c7cdbff2c001cc0d95096a2dde15a597
Ryan Bulcher <ryanbulcher@Ryans-MacBook-Pro.local>
2021-02-21 21:20:12 -0500

    Testing mail funciton

---
+++ php/contact.php
@@ -0,0 +1,16 @@
+<?php 
+if(isset($_POST['submit'])){
+    $to = "rjbulcher@gmail.com"; // this is your Email address
+    $from = $_POST['email']; // this is the sender's Email address
+    $first_name = $_POST['first_name'];
+    $last_name = $_POST['last_name'];
+    $subject = "Form submission";
+    $message = $first_name . " " . $last_name . " wrote the following:" . "\n\n" . $_POST['message'];
+
+    $headers = "From:" . $from;
+
+    $mail_sent = mail($to,$subject,$message,$headers);
+    header("Location: index.html");
+
+}
+?>

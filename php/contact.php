1905348263b152c26c084482a52f14173b90310d
Ryan Bulcher <ryanbulcher@Ryans-MacBook-Pro.local>
2021-02-21 21:35:35 -0500

    Testing mail funciton

--- php/contact.php
+++ php/contact.php
@@ -5,12 +5,16 @@ if(isset($_POST['submit'])){
     $first_name = $_POST['first_name'];
     $last_name = $_POST['last_name'];
     $subject = "Form submission";
+    $subject2 = "Copy of your form submission";
     $message = $first_name . " " . $last_name . " wrote the following:" . "\n\n" . $_POST['message'];
+    $message2 = "Here is a copy of your message " . $first_name . "\n\n" . $_POST['message'];
 
     $headers = "From:" . $from;
-
-    $mail_sent = mail($to,$subject,$message,$headers);
-    header("Location: index.html");
-
-}
+    $headers2 = "From:" . $to;
+    mail($to,$subject,$message,$headers);
+    mail($from,$subject2,$message2,$headers2); // sends a copy of the message to the sender
+    echo "Mail Sent. Thank you " . $first_name . ", we will contact you shortly.";
+    // You can also use header('Location: thank_you.php'); to redirect to another page.
+    // You cannot use header and echo together. It's one or the other.
+    }
 ?>

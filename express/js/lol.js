3e56aea4ad0aa23bd16b2b33886444957151f7c6
Ryan Bulcher <rjbulcher@gmail.com>
2021-10-07 14:07:21 -0400

    Update LoL page

--- express/js/lol.js
+++ express/js/lol.js
@@ -1,26 +1,26 @@
 //RIOT API KEY => RGAPI-ef9397bf-99b9-4847-b2c4-37bcb707c489
 
 //url example => https://na1.api.riotgames.com/lol/summoner/v4/summoners/by-name/Doublelift?api_key=RGAPI-YOUR-API-KEY
+// => https://na1.api.riotgames.com/lol/summoner/v4/summoners/by-name/Blighties?api_key=RGAPI-2990cfc3-97f5-4f3e-a403-4960218827ef
 
-const API_KEY = "RGAPI-ef9397bf-99b9-4847-b2c4-37bcb707c489"
-const url = "https://na1.api.riotgames.com/lol/summoner/v4/summoners/by-name/Doublelift?api_key="
-// function getLoLData() {
-//     console.log("Hello")
-//     var xhr = new XMLHttpRequest();
-//     xhr.open('GET', url + API_KEY, true);
-//     xhr.responseType = 'json';
-//     xhr.onload = function() {
-//       var status = xhr.status;
-//       if (status === 200) {
-//         callback(null, xhr.response);
-//       } else {
-//         callback(status, xhr.response);
-//       }
-//     };
-//     xhr.send();
-// }
-
-
+const API_KEY = "RGAPI-ef9397bf-99b9-4847-b2c4-37bcb707c489";
+const url = "https://na1.api.riotgames.com/lol/summoner/v4/summoners/by-name/";
 function getLoLData() {
-    console.log("Hello")
+  var searchName = document.getElementById("n2").value;
+  var getReq = url + searchName + "?api_key=" + API_KEY;
+  console.log(getReq);
+  fetch(getReq) //fetch the url
+    .then(
+      function (response) {
+        return response.json();
+      } //parse the response as JSON
+    )
+    .then(
+      function (json) {
+        console.log(json);
+      } //do something with the JSON
+    )
+    .catch(function (error) {
+      console.log(error);
+    });
 }
